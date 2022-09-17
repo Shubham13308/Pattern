03f4b6d90a540678f0f392b1da210d6e55ad16e2
Shubham13308 <103982047+Shubham13308@users.noreply.github.com>
2022-09-17 19:34:52 +0530

    Half_pyramid_With_Number

---
+++ src/Half_pyramid_With_Number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Half_pyramid_With_Number {
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
